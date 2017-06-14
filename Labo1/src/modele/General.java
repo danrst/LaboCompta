cc81ed72a11c5f30a2fe324a8dd98520af33d6c1
DaN <danesh.rostaei@gmail.com>
2017-06-14 11:52:38 +0200

    sénario vide

--- Labo1/src/modele/General.java
+++ Labo1/src/modele/General.java
@@ -1,7 +1,20 @@
 package modele;
 
+import java.util.TreeMap;
+
 public class General {
 
+	private TreeMap<String, String> compteGeneral;
+	private String numero;
+	private String libelle;
+	private char position;
+	private boolean sub;
 	
-	
+	public General() {
+		this.compteGeneral = new TreeMap<>();
+		this.numero = "";
+		this.libelle = "";
+		this.position = ' ';
+		this.sub = false;
+	}
 }
