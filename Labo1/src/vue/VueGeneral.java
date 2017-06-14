f5511d02f0f43179d483a415a2830548034743ea
DaN <danesh.rostaei@gmail.com>
2017-06-14 11:42:12 +0200

    Création de test

---
+++ Labo1/src/vue/VueGeneral.java
@@ -0,0 +1,11 @@
+package vue;
+
+import be.steformations.training.certification.controle.EtatApplication;
+
+public class VueGeneral {
+
+	public String getScreen() {
+		StringBuilder generalBuild;
+		generalBuild = new StringBuilder();
+		EtatApplication state = ui.getState();
+}
